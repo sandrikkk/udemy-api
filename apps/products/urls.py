a8d4b8a56128ead75ded37477880083552edb190
sandro <iashvilisandro7@gmail.com>
2023-06-25 13:23:46 +0400

    reviews

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from apps.products import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name="products"),
-    path('<int:pk>/', views.ProductDetail.as_view())
+    path("", views.ProductList.as_view(), name="products"),
+    path("<int:pk>/", views.ProductDetail.as_view()),
 ]
