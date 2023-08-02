dd494f84fc409202fff510079314848e37c9362b
sandro <iashvilisandro7@gmail.com>
2023-08-02 13:04:00 +0400

    fix all errors of swagger schema

--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import get_object_or_404
-from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from apps.products.models import Product
 from apps.reviews.permissions import OrderPermission
 from apps.reviews.serializer import ReviewSerializer
@@ -13,6 +10,7 @@ from apps.reviews.serializer import ReviewSerializer
 
 class Reviews(APIView):
     queryset = Product.objects.all()
+    serializer_class = ReviewSerializer
 
     def get_permissions(self):
         if self.request.method == "GET":
@@ -27,39 +25,15 @@ class Reviews(APIView):
     def get(self, request, pk):
         product = get_object_or_404(Product, id=pk)
         reviews = product.reviews.all()
-        serializer = ReviewSerializer(reviews, many=True)
+        serializer = self.serializer_class(reviews, many=True)
         return Response(serializer.data)
 
     def post(self, request, pk):
         pk = self.kwargs["pk"]
-        serializer = ReviewSerializer(
+        serializer = self.serializer_class(
             data=request.data,
             context={"request": request, "pk": pk, "user": request.user},
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    
-# class OrderAPIView(CreateAPIView):
-#     serializer_class = ReviewSerializer
-#     queryset = Product.objects.all()
-#     permission_classes = [
-#         IsAuthenticated,
-#     ]
-# 
-#     def get_permissions(self):
-#         if self.request.method == "GET":
-#             return [
-#                 OrderPermission(),
-#             ]
-#         else:
-#             return [
-#                 IsAuthenticated(),
-#             ]
-# 
-#     def get_serializer_context(self):
-#         context = super().get_serializer_context()
-#         context["pk"] = self.kwargs.get("pk")
-#         context['user']
-#         return context
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import permissions, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -6,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.users.models import User
 from apps.users.serializer import UserSerializer
+from udemy.openapi_responses import register_view_post_responses
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -19,9 +21,10 @@ class LoginUser(TokenObtainPairView):
 class RegisterUser(APIView):
     queryset = User.objects.all()
     permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -30,8 +33,9 @@ class RegisterUser(APIView):
 
 class GetUserProfile(APIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
 
     def get(self, request):
         user = request.user
-        serializer = UserSerializer(user)
+        serializer = self.serializer_class(user)
         return Response(serializer.data)
