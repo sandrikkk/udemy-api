f75dfcaafd4d0e95d759ca1d1935f23e41b2b7e9
sandro <iashvilisandro7@gmail.com>
2023-08-15 12:07:34 +0400

    fields in product serializer

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -6,4 +6,14 @@ from apps.products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = [
+            "id",
+            "created_at",
+            "updated_at",
+            "deleted",
+            "name",
+            "description",
+            "price",
+            "images",
+            "category",
+        ]
