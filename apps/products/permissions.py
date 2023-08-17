4b1c9f1727806851ba972162c9dc68c20375fb94
sandro <iashvilisandro7@gmail.com>
2023-08-17 16:27:47 +0400

    make more cleaner

--- apps/products/permissions.py
+++ apps/products/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class CustomPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method == SAFE_METHODS:
+        if request.method in SAFE_METHODS:
             return True
         else:
             return request.user and request.user.is_authenticated
