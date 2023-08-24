f79cb8e656ec3b789d999fb3acdf83dac82c6637
sandro <iashvilisandro7@gmail.com>
2023-08-24 15:39:21 +0400

    add UniqueTogetherValidator for review

--- apps/reviews/serializer.py
+++ apps/reviews/serializer.py
@@ -12,28 +12,22 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ["product", "comment", "star", "user"]
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Review.objects.all(), fields=("product", "user"), message="Product has already been reviewed."
+            )
+        ]
 
     def validate_user(self, user):
         if not user.is_verified:
-            raise serializers.ValidationError(
-                "In order to leave a review, your profile must first be verified."
-            )
+            raise serializers.ValidationError("In order to leave a review, your profile must first be verified.")
         return user
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
         product = attrs.get("product")
         user = self.context.get("user")
-        order = Order.objects.filter(
-            user=user, product_id=product.id, status=Status.SUCCEED
-        ).first()
+        order = Order.objects.filter(user=user, product_id=product.id, status=Status.SUCCEED).first()
         if not order:
-            raise serializers.ValidationError(
-                "You haven't purchased the associated product to review this product."
-            )
-        already_exist = product.reviews.filter(user=user).exists()
-        if already_exist:
-            raise serializers.ValidationError(
-                {"detail": "Product has already been reviewed."}
-            )
+            raise serializers.ValidationError("You haven't purchased the associated product to review this product.")
         return attrs
